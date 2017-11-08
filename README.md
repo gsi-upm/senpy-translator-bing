f6c9e414cb98a5808adc3dd5fb6b1358d03e1fb9
Eduardo Varas <e.varas@alumnos.upm.es>
2017-11-08 12:48:33 +0100

    Functional Translator Plugin

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Arabian Bing Translator
+
+Plugin for arabian to english translation.
+
+An API key from portal.azure.com is needed to create a token . See Microsoft Translator API Documentation: http://docs.microsofttranslator.com/. 
---
+++ translator.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+
+import time
+import logging
+import requests
+import json
+import string
+import os
+from os import path
+import time
+from senpy.plugins import SentimentPlugin, AnalysisPlugin
+from senpy.models import Results, Entry, Sentiment, Error
+from mstranslator import Translator
+
+logger = logging.getLogger(__name__)
+
+
+class TranslatorPlugin(AnalysisPlugin, Translator):
+    
+
+    def analyse_entry(self, entry, params):
+
+        txt = entry['nif:isString']
+        lang = params.get("lang")
+        key = params.get("k")
+        print(params)
+        translator = Translator(key)
+        txttranslated = translator.translate(txt, lang_from=lang, lang_to='en')
+        entry['output'] = txttranslated
+
+        yield entry
+
+       
