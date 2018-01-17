ab9d08a99ebb50352dee502a354e349d0f4e4569
J. Fernando Sánchez <balkian@gmail.com>
2018-01-17 11:35:34 +0100

    Doing things properly

--- translator.py
+++ translator.py
@@ -15,19 +15,16 @@ from mstranslator import Translator
 logger = logging.getLogger(__name__)
 
 
-class TranslatorPlugin(AnalysisPlugin, Translator):
-    
+class TranslatorPlugin(AnalysisPlugin):
+    '''Plugin que traduce de cualquier idioma al ingles'''
 
     def analyse_entry(self, entry, params):
-
         txt = entry['nif:isString']
         lang = params.get("lang")
-        key = params.get("k")
+        key = params.get("key")
         print(params)
         translator = Translator(key)
         txttranslated = translator.translate(txt, lang_from=lang, lang_to='en')
         entry['output'] = txttranslated
 
         yield entry
-
-       
